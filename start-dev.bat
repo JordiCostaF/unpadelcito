2eda6125f1ba62b63663761f59b35a4a08f610c2
Jordi <Jordicf937@gmail.com>
2025-07-02 19:44:39 +0000

    cuando inicio start-dev me abre una pagina y me aparece No se puede con

--- start-dev.bat
+++ start-dev.bat
@@ -1,26 +1,35 @@
 @echo off
-title Starting UnPadelazo Dev Servers
+REM --- Check for and kill existing processes on ports 3000 and 4000 ---
+echo Searching for processes on ports 3000 and 4000...
+for /f "tokens=5" %%a in ('netstat -aon ^| findstr ":3000" ^| findstr "LISTENING"') do (
+    echo Killing process with PID %%a on port 3000
+    taskkill /F /PID %%a
+)
+for /f "tokens=5" %%a in ('netstat -aon ^| findstr ":4000" ^| findstr "LISTENING"') do (
+    echo Killing process with PID %%a on port 4000
+    taskkill /F /PID %%a
+)
+echo Done.
 
-echo Starting Next.js and Genkit servers in the background...
+REM --- Start servers in the background ---
+echo Starting Next.js dev server and Genkit server in the background...
 
-:: Create a temporary VBScript to run commands hidden
->%temp%\runhidden.vbs echo Set WshShell = CreateObject("WScript.Shell")
->>%temp%\runhidden.vbs echo WshShell.Run "npm run dev", 0, false
->>%temp%\runhidden.vbs echo WshShell.Run "npm run genkit:dev", 0, false
+>"%temp%\run_hidden.vbs" echo Set WshShell = CreateObject("WScript.Shell")
+>>"%temp%\run_hidden.vbs" echo WshShell.Run "npm run dev", 0
+>>"%temp%\run_hidden.vbs" echo WshShell.Run "npm run genkit:dev", 0
 
-:: Execute the VBScript
-cscript //nologo %temp%\runhidden.vbs
+cscript //nologo "%temp%\run_hidden.vbs"
+del "%temp%\run_hidden.vbs"
 
-:: Delete the temporary VBScript
-del %temp%\runhidden.vbs
+echo Servers are starting... Please wait a moment.
 
-echo Servers are starting. Waiting a few seconds for them to be ready...
-:: Wait for 5 seconds to give servers time to start
-timeout /t 5 /nobreak > nul
+REM --- Wait for the server to initialize (increased to 15 seconds) ---
+echo Waiting for servers to start... (15 seconds)
+timeout /t 15 /nobreak >nul
 
-echo Opening project in browser at http://localhost:3000
-:: Open the browser
-start "" http://localhost:3000
+REM --- Open the browser ---
+echo Opening project in browser...
+start http://localhost:3000
 
-echo Done. The servers are running in the background.
+echo Launch script finished. The servers are running in the background.
 echo Use stop-dev.bat to stop them.
